c7ab7404a5e5fd37fd0dedfbbca02600a842a31f
Mujahed-Khan <pmujahedk@gmail.com>
2021-01-27 14:57:02 +0530

    First-app
    fisrt-day

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import react from "react";
+export const Header = () => {
+    return(
+        <div className="Nav">
+            
+            <nav>
+                <a href="">Home</a>
+                <a href="">Services</a>
+                <a href="">About</a>
+                <a href="">Contact</a>
+            </nav>
+            
+        </div>
+
+    );
+};
